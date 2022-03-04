7a8fa995236a55495820457c3ddc3495b536fb60
yungdanie <nikoches253@gmail.com>
2022-03-04 16:05:47 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,13 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setAge(21);
+        student.setName("Daniel");
+        student.setSurName("Agentov");
+        student.setFaculty("Energeticheskiy");
+        student.setSpec("Svyaz");
+        System.out.println("Имя студента " + student.getName() + ", фамилия студента " + student.getSurName() + ", факультет студента " + student.getFaculty());
+    }
+}
