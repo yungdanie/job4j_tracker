8d06bbb6236c7f7a44099998d74e5275f26066a7
yungdanie <nikoches253@gmail.com>
2022-03-07 11:58:58 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #272061]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -60,13 +60,11 @@ public class Tracker {
     public boolean delete(int id) {
         int index = indexOf(id);
         if (index != -1) {
-            items[index] = null;
             System.arraycopy(items, index + 1, items, size - index - 1, index);
             items[size - 1] = null;
             size--;
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 }
