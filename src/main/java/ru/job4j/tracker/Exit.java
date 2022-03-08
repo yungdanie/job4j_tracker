8a45790bf0feed65c8c69d59bbe0fc8e27ee5aaf
yungdanie <nikoches253@gmail.com>
2022-03-08 15:37:33 +0300

    8. Реализация меню за счет шаблона стратегия. [#181780]

---
+++ src/main/java/ru/job4j/tracker/Exit.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker;
+
+public class Exit implements UserAction {
+    @Override
+    public String name() {
+        return "Exit";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("Выход из программы");
+        return false;
+    }
+}
