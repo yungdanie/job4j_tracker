b9a1d2b3e32f2a6fd87e8e7d5d994fb2feb38c85
yungdanie <nikoches253@gmail.com>
2022-03-14 17:01:19 +0300

    2.1. Тест на ValidateInput [#311887 #274062]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -33,21 +33,16 @@ public class ValidateInputTest {
     public void whenMultiValidInput() {
         String[] array = {"1", "2", "51", "100"};
         Output out = new StubOutput();
-        Input in = new StubInput(
-                array
-        );
+        Input in = new StubInput(array);
         ValidateInput input = new ValidateInput(out, in);
-        int[] actual = new int[array.length];
-        int[] expected = new int[array.length];
-        actual[0] = input.askInt("Enter menu:");
-        actual[1] = input.askInt("Enter menu:");
-        actual[2] = input.askInt("Enter menu:");
-        actual[3] = input.askInt("Enter menu:");
-        expected[0] = Integer.parseInt(array[0]);
-        expected[1] = Integer.parseInt(array[1]);
-        expected[2] = Integer.parseInt(array[2]);
-        expected[3] = Integer.parseInt(array[3]);
-        assertThat(expected, is(actual));
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+        selected = input.askInt("Enter menu:");
+        assertThat(selected, is(2));
+        selected = input.askInt("Enter menu:");
+        assertThat(selected, is(51));
+        selected = input.askInt("Enter menu:");
+        assertThat(selected, is(100));
     }
 
     @Test
