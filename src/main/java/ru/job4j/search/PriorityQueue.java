3a05c36b59ec7394a8780b1621a73d8eec9b4e46
yungdanie <nikoches253@gmail.com>
2022-03-16 15:46:58 +0300

    2. Очередь с приоритетом на LinkedList [#41670 #275651]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -10,6 +10,8 @@ public class PriorityQueue {
         for (Task element : tasks) {
             if (element.getPriority() < task.getPriority()) {
                 index++;
+            } else {
+                break;
             }
         }
         this.tasks.add(index, task);
