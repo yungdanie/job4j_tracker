8cfda30587b520ea93f62e70a2a81e5c233a85f3
yungdanie <nikoches253@gmail.com>
2022-03-16 18:45:37 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String original : origin) {
+            check.add(original);
+            for (String dupl : text) {
+                if (check.contains(dupl)) {
+                    break;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return rsl;
+    }
+}
