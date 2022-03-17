321580b3c504b75268c17b39fe20d86b7e86140e
yungdanie <nikoches253@gmail.com>
2022-03-17 13:36:46 +0300

    2. Идентичные тексты [#212655 #276123]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 
 public class UniqueText {
     public static boolean isEquals(String originText, String duplicateText) {
-        boolean rsl = true;
+        boolean rsl = false;
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>();
@@ -15,6 +15,7 @@ public class UniqueText {
                     rsl = false;
                     break;
                 }
+                rsl = true;
             }
         }
         return rsl;
