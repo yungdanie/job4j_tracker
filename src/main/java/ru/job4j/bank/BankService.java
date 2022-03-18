c6191f229f118cf0587f8bb5198b63fddd78ef3b
yungdanie <nikoches253@gmail.com>
2022-03-18 13:17:59 +0300

    3. Банковские переводы [#10038 #276499]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -9,21 +9,18 @@ public class BankService {
     private final Map<User, List<Account>> users = new HashMap<>();
 
     public void addUser(User user) {
-        if (!users.containsKey(user)) {
-            users.put(user, new ArrayList<>());
-        } else {
-            System.out.println("Пользователь уже существует в системе");
-        }
+        users.putIfAbsent(user, new ArrayList<>());
     }
 
     public void addAccount(String passport, Account account) {
-        List<Account> accounts = users.get(findByPassport(passport));
-        for (Account acc : accounts) {
-            if (acc.equals(account)) {
-                System.out.println("Такой счёт уже существует у пользователя");
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> accounts = users.get(user);
+            if (!accounts.contains(account)) {
+                accounts.add(account);
             }
         }
-        accounts.add(account);
+
     }
 
     public User findByPassport(String passport) {
@@ -36,8 +33,9 @@ public class BankService {
     }
 
     public Account findByRequisite(String passport, String requisite) {
-        if (findByPassport(passport) != null) {
-            List<Account> accounts = users.get(findByPassport(passport));
+        User user = findByPassport(passport);
+        if (user != null) {
+            List<Account> accounts = users.get(user);
             for (Account acc : accounts) {
                 if (acc.getRequisite().equals(requisite)) {
                     return acc;
@@ -49,19 +47,15 @@ public class BankService {
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
-        if (findByRequisite(srcPassport, srcRequisite) != null
-                && findByRequisite(destPassport, destRequisite) != null) {
-            Account srcAcc = findByRequisite(srcPassport, srcRequisite);
-            Account destAcc = findByRequisite(destPassport, destRequisite);
+        Account srcAcc = findByRequisite(srcPassport, srcRequisite);
+        Account destAcc = findByRequisite(destPassport, destRequisite);
+        if (srcAcc != null
+                && destAcc != null) {
             if (srcAcc.getBalance() >= amount) {
                 srcAcc.setBalance(srcAcc.getBalance() - amount);
                 destAcc.setBalance(destAcc.getBalance() + amount);
                 return true;
-            } else {
-                System.out.println("Недосаточно средств на счете!");
             }
-        } else {
-            System.out.println("Пользователи/Пользователь с указаннным пасспортом/ами не найдены");
         }
         return false;
     }
