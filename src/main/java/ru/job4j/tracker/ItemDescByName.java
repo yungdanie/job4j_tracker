aad4b7baacb02aa9e02d93d0b90856420a5a2446
yungdanie <nikoches253@gmail.com>
2022-03-19 15:15:55 +0300

    0. Сортировка [#10096 #276920]

--- src/main/java/ru/job4j/tracker/ItemDescByName.java
+++ src/main/java/ru/job4j/tracker/ItemDescByName.java
@@ -6,6 +6,6 @@ public class ItemDescByName implements Comparator<Item> {
 
     @Override
     public int compare(Item o1, Item o2) {
-        return -o1.getName().compareTo(o2.getName());
+        return o2.getName().compareTo(o1.getName());
     }
 }
