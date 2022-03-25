7f5b58be511eed11c45dae61293a6a2a38f76adf
yungdanie <nikoches253@gmail.com>
2022-03-25 15:48:13 +0300

    2. Список адресов [#110062]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -6,6 +6,9 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+        return profiles.stream().
+                map(Profile::getAddress).
+                collect(Collectors.toList());
     }
+
 }
