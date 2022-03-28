644fcea0ebb0d26e9259283fa58adeb54d33f787
yungdanie <nikoches253@gmail.com>
2022-03-28 14:47:48 +0300

    2. Список адресов [#110062 #280259]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -7,17 +7,16 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().
-                map(Profile::getAddress).
-                collect(Collectors.toList());
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .collect(Collectors.toList());
     }
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
-        return profiles.
-                stream().
-                map(Profile::getAddress).
-                sorted(Comparator.comparing(Address::getCity)).
-                distinct().
-                collect(Collectors.toList());
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
