1d29bf90e5157fb62a2787bd6bd314f566b38e22
yungdanie <nikoches253@gmail.com>
2022-03-28 17:42:31 +0300

    0. Stream API улучшения [#58167]

--- src/main/java/ru/job4j/stream/Student.java
+++ src/main/java/ru/job4j/stream/Student.java
@@ -3,12 +3,11 @@ package ru.job4j.stream;
 import java.util.Objects;
 
 public class Student {
-    private int score;
 
+    private int score;
     private String surname;
 
-    public Student(int score, String surname) {
-        this.score = score;
+    public void setSurname(String surname) {
         this.surname = surname;
     }
 
@@ -18,6 +17,11 @@ public class Student {
 
     public String getSurname() {
         return surname;
+    }
+
+    public Student(String surname, int score) {
+        this.score = score;
+        this.surname = surname;
     }
 
     @Override
@@ -30,11 +34,12 @@ public class Student {
         }
         Student student = (Student) o;
         return score == student.score
-                && Objects.equals(surname, student.surname);
+                && surname.equals(student.surname);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(score, surname);
     }
+
 }
