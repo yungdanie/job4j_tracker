8a16ccaa0a83f850081d5138a9ed94c7a56790bf
yungdanie <nikoches253@gmail.com>
2022-09-07 18:08:34 +0300

    1. Lombok. Настройка IDEA [#504888]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,87 +1,14 @@
 package ru.job4j.tracker;
 
-import java.sql.Timestamp;
+import lombok.Data;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
 
+@Data
 public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-    public Item() {
-    }
-
-    public Item(int id) {
-        this.id = id;
-    }
-
-    public Item(String name) {
-        this.name = name;
-    }
-
-    public Item(String name, int id) {
-        this.name = name;
-        this.id = id;
-    }
-
-    public Item(String name, int id, LocalDateTime date) {
-        this.name = name;
-        this.id = id;
-        this.created = date;
-    }
-
-    public String getDataTime() {
-        return created.format(FORMATTER);
-    }
-
-    public void setDataTime(Timestamp timestamp) {
-        if (timestamp != null) {
-            created = timestamp.toLocalDateTime();
-        }
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Item{"
-                + "id=" + id
-                + ", name='" + name + '\''
-                + ", created=" + created.format(FORMATTER)
-                + '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Item item = (Item) o;
-        return id == item.id && Objects.equals(name, item.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, created);
-    }
 }
